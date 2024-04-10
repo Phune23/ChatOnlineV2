c82a974ec077d301a5a5f2b9495af819b8819400
Phune23 <phutranbs23@gmail.com>
2024-04-10 23:13:28 +0700

    update 1: add migration,identity

---
+++ ChatOnlineV2/Data/Entities/ManageUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ChatOnlineV2.Data.Entities
+{
+    public class ManageUser : IdentityUser
+    {
+        public string DisPlayName { get; set; }
+        public DateTime BrithDay { get; set; }
+    }
+}
