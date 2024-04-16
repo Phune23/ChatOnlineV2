6a396025767291a22efe61001e3a37565870aaef
Phune23 <phutranbs23@gmail.com>
2024-04-17 01:17:31 +0700

    Add some avatar

--- ChatOnlineV2/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ChatOnlineV2/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -77,6 +77,16 @@ namespace ChatOnlineV2.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 5)]
+            [Display(Name = "UserName")]
+            public string UserName { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 5)]
+            [Display(Name = "FullName")]
+            public string FullName { get; set; }
+
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
@@ -101,6 +111,11 @@ namespace ChatOnlineV2.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+
+            [Required]
+            [Display(Name = "Avatar")]
+            public string Avatar { get; set; }
         }
 
 
